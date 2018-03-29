e2b1130c4d5d320c35a8793d6b75afbf30eaac3f
Michael Wu <mw10013@gmail.com>
2018-03-29 16:21:46 -0400

    layout

--- src/cljs/antd_demo/views.cljs
+++ src/cljs/antd_demo/views.cljs
@@ -14,17 +14,36 @@
     "Example taken from https://codesandbox.io/s/jnw46698m3"]
    [:> antd.DatePicker {:defaultValue (m)}]])
 
+(defn layout-example []
+  [:> antd.Layout
+   [:> antd.Layout.Sider "Sider"]
+   [:> antd.Layout
+    [:> antd.Layout.Header "Header"]
+    [:> antd.Layout.Content "Content"]
+    [:> antd.Layout.Footer "Footer"]]])
+
 (defn home-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div (str "Hello from " @name ". This is the Home Page.")
      [date-picker-example]
+     [:div {:style {:padding 24}}
+      [layout-example]]
      [:div [:a {:href "#/about"} "go to About Page"]]]))
 
 
 ;; about
 
+;http://imghst.co/95/tLLy~YWL8D.jpg
+;<img src="https://s1.gifyu.com/images/baby-boy-heart-background.jpg" alt="" width="615" height="615" data-load="full" style="">
+
 (defn about-panel []
   [:div "This is the About Page."
+   [:div {:style {#_:background-color #_"lightblue"
+                  :background "lightblue url('http://imghst.co/95/tLLy~YWL8D.jpg') no-repeat right top"
+                  :padding    24
+                  :margin     12
+                  :width      615
+                  :height     615}} "Background Image Test"]
    [:div [:a {:href "#/"} "go to Home Page"]]])
 
 
